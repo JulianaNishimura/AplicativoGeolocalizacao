1a058efba721c3722d8b1857f2c3242ae322dbcf
JulianaNishimura <juliana.nishimura.tds24@gmail.com>
2025-09-18 13:57:20 -0300

    arrumando merge

--- GeoApp/index.js
+++ GeoApp/index.js
@@ -1,9 +1,5 @@
 import { registerRootComponent } from "expo";
 
-<<<<<<< HEAD
-import App from '.src/App';
-=======
 import App from "./src/App";
->>>>>>> 7714afb6998187a9171d866a3a1c81f6a13cc061
 
 registerRootComponent(App);
--- GeoApp/src/App.js
+++ GeoApp/src/App.js
@@ -1,23 +1,8 @@
-<<<<<<< HEAD
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import 'react-native-gesture-handler';
-
-const Tab = createBottomTabNavigator();
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
-=======
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet } from "react-native";
 import TelaInicial from "../screens/TelaInicial";
 export default function App() {
   return (
@@ -25,6 +10,5 @@ export default function App() {
       <StatusBar style="light" backgroundColor="#131317ff" />
       <TelaInicial />
     </>
->>>>>>> 7714afb6998187a9171d866a3a1c81f6a13cc061
   );
 }
